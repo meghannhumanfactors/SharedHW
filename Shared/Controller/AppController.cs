f05f46d6ea952ea754e8d3fd85c72497421621af
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2019-06-08 12:36:40 -0700

    .

---
+++ Shared/Controller/AppController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Shared.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
